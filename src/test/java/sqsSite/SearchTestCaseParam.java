b8fd1ef0415b9f8762d40b8b913ec6640431a8cc
unknown <careys@NCND3450C9W.en.group.intl>
2015-05-18 23:31:07 +0100

    adding pom

---
+++ src/test/java/sqsSite/SearchTestCaseParam.java
@@ -0,0 +1,44 @@
+package sqsSite;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver; 
+
+import sqsSite.pages.HomePage;
+import sqsSite.pages.SearchPage;
+import junitparams.*;
+
+@RunWith(JUnitParamsRunner.class)
+public class SearchTestCaseParam {
+
+    WebDriver driver;
+    String searchTerm = "SQS";
+    String countryToSelect = "Ireland";
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.get("http://www.sqs.com");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+
+    @Test
+    @Parameters({"selenium, 1","SQS, 25","test, 162"})
+    public void searchTestCase(String searchTerm,int searchCount) {
+        HomePage home = new HomePage(driver);
+        SearchPage search = home.searchFor(searchTerm);
+        assertEquals("http://www.sqs.com/en-group/_meta/search.php?search_item="+searchTerm,driver.getCurrentUrl());
+        assertEquals("The search count for "+searchTerm+" didnt match",searchCount,search.searchResult());
+    }
+    
+}
